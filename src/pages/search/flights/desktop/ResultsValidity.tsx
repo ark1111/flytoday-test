279e382b96d141f0ce245ddce2547983e552d4de
ark1111 <alireza.khaji11@gmail.com>
2023-10-05 16:08:01 +0330

    feat : ResultsValidity

--- src/pages/search/flights/desktop/ResultsValidity.tsx
+++ src/pages/search/flights/desktop/ResultsValidity.tsx
@@ -1,14 +1,43 @@
-import React from "react";
+import CircularProgress from "@mui/material/CircularProgress";
+import { useState, useEffect } from "react";
 
 type Props = {};
 
 const ResultsValidity = (props: Props) => {
+  const totalTime = 300;
+  const [timer, setTimer] = useState(totalTime);
+
+  useEffect(() => {
+    let newInterval = setInterval(() => {
+      if (timer > 0) {
+        setTimer((oldTimer) => oldTimer - 1);
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(newInterval);
+    };
+  }, []);
+
   return (
     <div className="w-full flex items-center justify-between">
       <div className="text-[#464646] text-[14px]">مدت زمان اعتبار نتایج</div>
       <div className="flex items-center gap-x-1">
-        <div className="text-[14px] text-[#1773dc]">14:35</div>
-        <div className="w-[16px] h-[16px] border"></div>
+        <div className="text-[14px] text-[#1773dc]">
+          {Math.floor(timer / 60)}:
+          {timer - Math.floor(timer / 60) * 60 < 10 && "0"}
+          {timer - Math.floor(timer / 60) * 60}
+        </div>
+        <div className="w-[16px] h-[16px] flex items-center justify-center relative">
+          <div className="w-[15.5px] h-[15.5px] border-[1px] border-[#d0d0d0] absolute z-0 rounded-full"></div>
+          <CircularProgress
+            variant="determinate"
+            value={100 - (timer / totalTime) * 100}
+            size={16}
+            sx={{ color: "#1773dc" }}
+            thickness={5}
+          />
+        </div>
       </div>
     </div>
   );
